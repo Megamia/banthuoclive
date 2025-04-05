c8428e874c8d30f66a50d1a57c4c770e388bd51d
Megamia <luudat214@gmail.com>
2025-04-05 21:02:08 +0700

    update

--- BE/plugins/betod/livotec/controllers/revenue/_list_toolbar.php
+++ BE/plugins/betod/livotec/controllers/revenue/_list_toolbar.php
@@ -1,18 +1,23 @@
-<div data-control="toolbar">
+<!-- <div data-control="toolbar"> -->
     <div class="widget-body">
         <canvas id="revenueData"></canvas>
     </div>
-    <a href="<?= Backend::url('betod/livotec/revenue/create') ?>" class="btn btn-primary oc-icon-plus">
+    <!-- <a href="<?= Backend::url('betod/livotec/revenue/create') ?>" class="btn btn-primary oc-icon-plus">
         <?= e(trans('backend::lang.form.create')) ?>
     </a>
     <button class="btn btn-default oc-icon-trash-o" data-request="onDelete"
         data-request-confirm="<?= e(trans('backend::lang.list.delete_selected_confirm')) ?>" data-list-checked-trigger
         data-list-checked-request data-stripe-load-indicator>
         <?= e(trans('backend::lang.list.delete_selected')) ?>
-    </button>
+    </button> -->
 
 
-</div>
+<!-- </div> -->
+<style>
+    .list-widget-container {
+        display: none;
+    }
+</style>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
